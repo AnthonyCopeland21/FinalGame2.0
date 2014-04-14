3a75fd3db9bc164049804b50419a95a37de2e075
AnthonyCopeland21 <Anthony.Copeland21@gmail.com>
2014-04-14 08:45:24 -0400

    Here's the game

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public Vector3 newPos = new Vector3(0f,0f,0f);
+	private CharacterController controller;
+	private float gravity = -.2f;
+	private float sideSpeed = 10f;
+
+
+
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update () {
+		newPos.x = Input.GetAxis("Horizontal") * Time.deltaTime * sideSpeed;
+		newPos.y = Input.GetAxis("Vertical") * Time.deltaTime * sideSpeed;
+		//newPos.z += gravity * Time.deltaTime;
+		controller.Move(newPos);
+	}
+}
